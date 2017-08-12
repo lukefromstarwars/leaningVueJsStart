525642ec4700542d62143d441b85c4e5eb451d50
lukefromstarwars <lukefromstarwars@gmail.com>
2017-08-12 09:04:59 +0200

    038

--- src/main.js
+++ src/main.js
@@ -4,29 +4,9 @@ import Vue from 'vue'
 new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!',
-    title: 'Start here',
-    link: 'http://google.com',
-    finishedLink: '<a href="http://www.pfcsm-opgg.be">PFCSM</a>',
-    counter: 0,
-    x: 0,
-    y: 0
+    show: true
   },
   methods: {
-    sayHello: function () {
-      this.title = 'Pierre'
-      return this.title
-    },
-    increaseCount: function (step, event) {
-      this.counter += step
-      alert(event)
-    },
-    decreaseCount: function () {
-      this.counter--
-    },
-    updateCoord: function (event) {
-      this.x = event.clientX
-      this.y = event.clientY
-    }
+
   }
 })
