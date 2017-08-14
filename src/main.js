746f7d92b940006cf2275aa5f4085a992aa2f107
lukefromstarwars <lukefromstarwars@gmail.com>
2017-08-14 11:11:49 +0200

    094

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from "vue";
 
+var cmp = {
+  data: function() {
+    return {
+      status: "critical"
+    };
+  },
+  template:
+    "<p>servers status: {{status}} (<button type='text' @click='changeStatus'>change</button>)</p>",
+  methods: {
+    changeStatus: function() {
+      this.status = "New Status";
+    }
+  }
+};
+
 /* eslint-disable no-new */
 new Vue({
   el: "#app",
-  data: {},
-  methods: {}
+  components: {
+    "my-cmp": cmp
+  }
+});
+new Vue({
+  el: "#app2",
+  components: {
+    "my-cmp": cmp
+  }
 });
