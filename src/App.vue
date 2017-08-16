9c43d9e54494b62e8041f478fa0011e44cb54440
lukefromstarwars <lukefromstarwars@gmail.com>
2017-08-16 09:17:07 +0200

    163

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
-  <router-view></router-view>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Directives</h1>
+        <p v-text="'this is it'"></p>
+        <p v-html="'<strong>in bold</strong>this is it'"></p>
+
+      </div>
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <p v-hili:background.delayed="'red'" v-hili="'green'">Color this</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
-}
+};
 </script>
-<!--
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style> -->
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from "vue";
+import App from "./App.vue";
+
+Vue.directive("hili", {
+  bind (el, binding, vnode) {
+    // el.style.backgroundColor = "green";
+    // el.style.backgroundColor = binding.value;
+    var delay = 0;
+    if (binding.modifiers["delayed"]) {
+      delay = 1000;
+    }
+    setTimeout(function() {
+      if (binding.arg === "background") {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
+  }
+});
 
 /* eslint-disable no-new */
 new Vue({
   el: "#app",
-  data: {},
-  methods: {}
+  render: h => h(App)
 });
